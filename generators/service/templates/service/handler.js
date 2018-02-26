9a7dc64a7865fd0b3cc380c70ab1747bb8abfaba
imaustink <austin@austinkurpuis.com>
2018-02-25 18:44:57 -0800

    Added link to Response docs

--- generators/service/templates/service/handler.js
+++ generators/service/templates/service/handler.js
@@ -1,6 +1,6 @@
 const { Response } = require('warhead-<%= platform %>')
 
 module.exports = function (event) {
-  // Checkout the docs for Response
+  // Checkout the docs for Response (https://github.com/imaustink/warhead-<%= platform %>#responsebody)
   return Promise.resolve(new Response(event.body))
 }
